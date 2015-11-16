d0ce41918b46903ce54278d8173a0909f49e1bc0
ibeaumont <ibeaumont@zubirimanteo.com>
2015-11-16 20:03:17 +0100

    froga

--- js/app.js
+++ js/app.js
@@ -21,17 +21,19 @@ amigoApp.config(['$routeProvider',
   }]);    
 
    amigoApp.factory('amigosFact', function($resource){
-   var resource1= $resource('http://midenda-igor.herokuapp.com/producto:id', { id: '@_id' }, {
-    update: {method: 'PUT'}
-    });
-   var resource2= $resource('http://midenda-igor.herokuapp.com/producto:id', { id: '@_id' }, {
-    update: {method: 'PUT'}
-    });
    return {
       get: function(){
+        var resource1= $resource('http://midenda-igor.herokuapp.com/productos:id', { id: '@_id' }, {
+    update: {method: 'PUT'}
+    });
+   
           return resource1.query();
           },
       find: function(id){
+        var resource2= $resource('http://midenda-igor.herokuapp.com/producto:id', { id: '@_id' }, {
+    update: {method: 'PUT'}
+    });
+   
           return resource2.get({},{'_id': id});
       }
     };
